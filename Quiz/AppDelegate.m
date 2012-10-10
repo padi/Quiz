a38ff1d6a10e639073b21a2a82128bd3ddfa804c
Developer Account <dev@boracay.local>
2012-10-10 15:15:52 +0800

    - Added Main nib file to display window - implemented showQuestion, showAnswer, and added extra steps inside a custom init method - widened labels to fit longer questions

--- Quiz/AppDelegate.m
+++ Quiz/AppDelegate.m
@@ -12,6 +12,66 @@
 
 @synthesize window = _window;
 
+- (id)init
+{
+    //Call the init method implemented by the superclass
+    self = [super init];
+    if(self) {
+        //Create two arrays and make the pointers point to them
+        questions = [[NSMutableArray alloc] init];
+        answers = [[NSMutableArray alloc] init];
+        
+        //Add questions and answers to the arrays
+        [questions addObject:@"What is 7+7?"];
+        [answers   addObject:@"14"];
+        
+        [questions addObject:@"What is the capital of Vermont?"];
+        [answers   addObject:@"Montpelier"];
+        
+        [questions addObject:@"From what is cognac made?"];
+        [answers   addObject:@"Grapes"];
+    }
+    
+    //Return the address of the new object
+    return self;
+}
+
+- (IBAction)showQuestion:(id)sender
+{
+    //Step to the next question
+    currentQuestionIndex++;
+    
+    //Am I past the last question?
+    if (currentQuestionIndex == [questions count]) {
+        //Go back to first question
+        currentQuestionIndex = 0;
+    }
+    
+    //Get the string at that index in the questions array
+    NSString *question = [questions objectAtIndex:currentQuestionIndex];
+    
+    //Log the string to the console
+    NSLog(@"displaying question: %@", question);
+    
+    //Display the string in the question field
+    [questionField setText:question];
+    [questionField sizeToFit];
+    
+    //Clear the answer field
+    [answerField setText:@"???"];
+    [answerField sizeToFit];
+}
+
+- (IBAction)showAnswer:(id)sender
+{
+    //What is the answer to the current question?
+    NSString *answer = [answers objectAtIndex:currentQuestionIndex];
+    
+    //Display it in the answer field
+    [answerField setText:answer];
+    [answerField sizeToFit];
+}
+
 - (void)dealloc
 {
     [_window release];
@@ -22,7 +82,7 @@
 {
     //self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    //self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
