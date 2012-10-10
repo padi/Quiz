4bb38384bd445ca43edd5653edddd1428e63fdc6
Developer Account <dev@boracay.local>
2012-10-10 12:50:56 +0800

    Initial Commit

---
+++ Quiz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Steven Smith on 10/10/12.
+//  Copyright (c) 2012 AELOGICA LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
