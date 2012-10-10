87cb4e9d932ffea5f2c566490ccbbef24e902693
Developer Account <dev@boracay.local>
2012-10-10 13:53:33 +0800

    Add methods and outlets to App Delegate. Also changed added missing steps since "Window-based application" is missing in this Xcode version. For more information, see http://brianreiter.org/2011/11/23/how-to-create-an-xcode-4-0-style-window-based-application-in-xcode-4-2/

--- Quiz/AppDelegate.h
+++ Quiz/AppDelegate.h
@@ -6,10 +6,26 @@
 //  Copyright (c) 2012 AELOGICA LLC. All rights reserved.
 //
 
+//http://brianreiter.org/2011/11/23/how-to-create-an-xcode-4-0-style-window-based-application-in-xcode-4-2/
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
+{
+    int currentQuestionIndex;
+    
+    //model objects
+    NSMutableArray *questions;
+    NSMutableArray *answers;
+    
+    //the view objects - don't worry about this IBOutlet macro
+    //we'll talk about it shortly
+    IBOutlet UILabel *questionField;
+    IBOutlet UILabel *answerField;
+}
 
-@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+- (IBAction)showQuestion:(id)sender;
+- (IBAction)showAnswer:(id)sender;
 
 @end
